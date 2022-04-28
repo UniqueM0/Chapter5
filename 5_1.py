4462d0cb35900a50f4f33f2b4b549c99df2c7857
Unique Orduna <uniqueorduna@swuser-129-8-207-178.swlan.csufresno.edu>
2022-04-28 15:07:34 -0700

    more info

--- 5_1.py
+++ 5_1.py
@@ -1,38 +1,68 @@
 
+# file_name = "./infile.txt"
+# infile = open(file_name, 'r')
+# names = [line.rstrip() for line in infile]
+
+# print("infile", infile)
+
+# # Can use this or not
+# # for line in infile: 
+# #     print("Line: ", line)
+
+# names = [line.rstrip() for line in infile]
+# print("names: ", names)
+
+# infile.close()
+
+
+# def main():
+#     display_with_forloop(file_name)
+
+# def display_with_forloop(file_name):
+#     infile = open(file_name, 'r')
+#     for line in infile:
+#         print(line, end="")
+#     infile.close()
+
+
+# def display_with_list_comprehension(file_name):
+#     infile = open(file_name, 'r')
+#     items = [line.rstrip() for line in infile]
+#     print("items: ", items) 
+#     infile.close()
+
+
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+
+
+# main()
+
+
+
+#  Create a file object in write mode
+
 file_name = "./infile.txt"
 infile = open(file_name, 'r')
-
-print("infile", infile)
-
-# Can use this or not
-# for line in infile: 
-#     print("Line: ", line)
-
 names = [line.rstrip() for line in infile]
-print("names: ", names)
-
-infile.close()
-
 
 def main():
-    display_with_forloop(file_name)
-
-def display_with_forloop(file_name):
-    infile = open(file_name, 'r')
-    for line in infile:
-        print(line, end="")
-    infile.close()
-
-
-def display_with_list_comprehension(file_name):
-    infile = open(file_name, 'r')
-    items = [line.rstrip() for line in infile]
-    print("items: ", items) 
-    infile.close()
-
+    save_to_outfile("./outfile.txt")
 
 def open_file(file_name, mode):
     return open(file_name, mode)
 
+def save_to_outfile(file_name):
+    outfile = open("./outfile.txt", "w")
+    for name in names:
+        if "Doe" not in name:
+            print("Name to be persisted: ", name)
+            outfile.write(name + "\n")
+        else: 
+            print("Names to be excluded: ", name)
+    outfile.close()
 
 main()
+  
+    
+    
