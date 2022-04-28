1dd0e0822bacffa4a9d197e88e8483082dccbb46
Unique Orduna <uniqueorduna@swuser-129-8-207-178.swlan.csufresno.edu>
2022-04-28 15:24:06 -0700

    last for day

--- 5_1.py
+++ 5_1.py
@@ -42,27 +42,64 @@
 
 #  Create a file object in write mode
 
-file_name = "./infile.txt"
-infile = open(file_name, 'r')
-names = [line.rstrip() for line in infile]
+# file_name = "./infile.txt"
+# infile = open(file_name, 'r')
+# names = [line.rstrip() for line in infile]
 
-def main():
-    save_to_outfile("./outfile.txt")
+# def main():
+#     save_to_outfile("./outfile.txt")
 
-def open_file(file_name, mode):
-    return open(file_name, mode)
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
 
-def save_to_outfile(file_name):
-    outfile = open("./outfile.txt", "w")
-    for name in names:
-        if "Doe" not in name:
-            print("Name to be persisted: ", name)
-            outfile.write(name + "\n")
-        else: 
-            print("Names to be excluded: ", name)
-    outfile.close()
+# def save_to_outfile(file_name):
+#     outfile = open("./outfile.txt", "w")
+#     for name in names:
+#         if "Doe" not in name:
+#             print("Name to be persisted: ", name)
+#             outfile.write(name + "\n")
+#         else: 
+#             print("Names to be excluded: ", name)
+#     outfile.close()
 
-main()
+# main()
   
     
-    
+# a_list = []
+
+# a_set = {}
+
+# a_list.append("A")
+
+# a_set.add("A")
+# a_set.add("A")
+# a_set.add("B")
+# a_set.add("C")
+
+
+
+
+# def main():
+#     words = ["nudge", "nudge", "wink", "wink"]
+#     terms = set(words)
+#     print("terms", terms)
+#     terms.add("wink")
+#     terms.add("new wink")
+#     terms.discard("nudge")
+
+# main()
+
+
+arr1 = ["Alpha", "Bravo", "Charlie"]
+arr2 = ["Bravo", "Delta"]
+set1 = set(arr1)
+set2 = set(arr2)
+
+print("set1", set1)
+print("set2", set2)
+
+# built-in set methods: 1 .union, 1 .intersetion, 3 .difference()
+
+print("union ", set1.union(set2))
+print("intersection ", set1.intersection(set2))
+print("difference: ", set1.difference(set2))
